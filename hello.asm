f8d9eb5594858879e9f27f3b9b0e1d87458d1d00
silkthyme <christinahuangji@gmail.com>
2019-03-29 22:35:55 -0700

    added documentation to first line in hello.asm

--- hello.asm
+++ hello.asm
@@ -3,6 +3,8 @@ section .data
 	; "Hello World" is the bytes of data we are defining.
 	; Each character in the string of text is a single byte.
 	; The "10" is a newline character, "\n"
+	; The "text" is a name assigned to the address in memory that this data is located in.
+	; The compiler will replace all future instances of "text" with that memory address.
 	text db "Hello World!",10
 
 section .text
