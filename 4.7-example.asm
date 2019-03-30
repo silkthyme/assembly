650c8f436dabc8cadb805f9bc0cccefaa3ed0315
silkthyme <christinahuangji@gmail.com>
2019-03-30 00:26:44 -0700

    finished code section in 4.7-example.asm

--- 4.7-example.asm
+++ 4.7-example.asm
@@ -26,4 +26,41 @@ qVar1	dq	170000000
 qVar2	dq	90000000
 qResult	dq	0
 
-;
+; ************************************************************************************
+; code section
+section	.text
+global _start
+_start:
+	; performs a series of basic addition operations
+	
+	; byte example
+	; bResult = bVar1 + bVar2
+	mov	al, byte [bVar1]
+	add	al, byte [bVar2]
+	mov 	byte [bResult], al
+
+	; word example
+	; wResult = wVar1 + wVar2
+	mov	ax, word [wVar1]
+	add	ax, word [wVar2]
+	mov	word [wResult], ax
+
+	; double-word example
+	; dResult = dVar1 + dVar2
+	mov 	eax, dword [dVar1]
+	add	eax, dword [dVar2]
+	mov	dword [dResult], eax
+
+	; quad-word example
+	; qResult = qVar1 + qVar2
+	mov	rax, qword [qVar1]
+	add	rax, qword [qVar2]
+	mov	qword [qResult], rax
+	
+; ************************************************************************************
+; done, terminate program
+last:
+	mov	rax, SYS_exit		; call code for exit
+	mov 	rdi, EXIT_SUCCESS	; exit program with success
+	syscall
+
