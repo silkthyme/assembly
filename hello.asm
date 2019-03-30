8f89901b284d2d1f834df6a27426f154633e8e3e
silkthyme <christinahuangji@gmail.com>
2019-03-29 23:19:48 -0700

    further clarifying notes in hello.asm

--- hello.asm
+++ hello.asm
@@ -1,3 +1,4 @@
+; .data section, where all data is defined before compilation
 section .data
 	; db stands for define bytes, meaning define raw bytes of data to insert
 	; "Hello World" is the bytes of data we are defining.
@@ -7,10 +8,12 @@ section .data
 	; The compiler will replace all future instances of "text" with that memory address.
 	text db "Hello World!",10
 
+; .text section, where the actual code goes
 section .text
 	global _start
 
 _start:
+	; sys_write(1, text, 14)
 	; move the value 1 into the rax register
 	mov rax, 1
 	; move the filedescriptor into the rdi register, "1" means stdout
@@ -21,6 +24,7 @@ _start:
 	mov rdx, 13
 	syscall
 	
+	; sys_exit(0)
 	; sys_exit has an id of 60, move 60 into the rax register
 	mov rax, 60
 	; move 0, the errorcode, into rdi, indicating there was no error
